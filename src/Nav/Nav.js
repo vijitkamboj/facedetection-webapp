fc225f70841731642a1c3efe4c98c2fecffa7119
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-19 22:49:30 +0530

    added Nav

---
+++ src/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+
+function App() {
+  return (
+    <div id="nav">
+ 
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Nav from './Nav/Nav'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div id="container">
+    <Nav />
+</div>, document.getElementById('root'));
 
 
 
