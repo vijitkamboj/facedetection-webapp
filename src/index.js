4de56413d9be68a194e1808fa7a09ece05e0896b
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-20 11:19:54 +0530

    created separate file for particleOptions

--- src/index.js
+++ src/index.js
@@ -2,31 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Particles from 'react-particles-js'
+import particlesOptions from './particleOptions'
 import Nav from './Nav/Nav';
 import Logo from './Logo/Logo';
 import ImageForm from './ImageForm/ImageForm'
 import Rank from './Rank/Rank'
 
-const particlesOptions = {
-    particles: {
-        number: {
-            value:100,
-            density: {
-                enable: true,
-                value_area: 800
-            }
-        },line_linked: {
-            color: "#000000",
-        },
-        color: {
-            value: "#000000",
-        },shape: {
-            type: "polygon",
-        },size: {
-            value: 3
-        }
-    }
-}
+
 
 ReactDOM.render(<div id="container">
     <Particles className='particles' params={particlesOptions} />
---
+++ src/particleOptions.js
@@ -0,0 +1,22 @@
+const particlesOptions = {
+    particles: {
+        number: {
+            value:100,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },line_linked: {
+            color: "#000000",
+        },
+        color: {
+            value: "#000000",
+        },shape: {
+            type: "polygon",
+        },size: {
+            value: 3
+        }
+    }
+}
+
+export default particlesOptions
