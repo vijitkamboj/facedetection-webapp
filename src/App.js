716a73f39d68b44a0543b47467014ffcf77da9c6
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-20 14:56:06 +0530

    added input and submit events

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ import Rank from './Rank/Rank'
 
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state ={
+            input : ""
+        }  
+    }
+
+    onInputChange = (event) =>{
+        console.log(event.target.value)
+    }  
+
+    onSubmit = () => {
+        console.log("click")
+    }
+
     render(){
         return (
             <div id="container">
@@ -17,7 +32,7 @@ class App extends Component {
                 <Nav />
                 <Logo />
                 <Rank />
-                <ImageForm />
+                <ImageForm onInputChange = {this.onInputChange} onSubmit = {this.onSubmit}/>
             </div>
         );
     }
--- src/ImageForm/ImageForm.js
+++ src/ImageForm/ImageForm.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './ImageForm.css';
 
 
-function App() {
+function App({onInputChange,onSubmit}) {
   return (
     <div id="Form-cont">
         <p id="message">This app will detect faces in your pictures.Give it a try!!</p>
         <div id="input-cont">
-            <div id="deticon" ></div>
-            <input type="text" placeholder="Enter the url of image..."/>
+            <div id="deticon" onClick = {onSubmit} ></div>
+            <input type="text" placeholder="Enter the url of image..." onChange={onInputChange}/>
         </div>
     </div>
 
