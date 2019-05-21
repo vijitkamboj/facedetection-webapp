d115c26b0156a88f69ed5fa6191dfd3cb704e730
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-21 21:26:45 +0530

    added properties to forms

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -8,15 +8,15 @@ const App = ({route , onRouteChange}) => {
 				<div id="heading" >Register</div>
 				<div className="field">
 					<p className="label">Name</p>
-					<input type="text" className="form"/>
+					<input type="text" className="form" name="name"/>
 				</div>
 				<div className="field">
 					<p className="label">Email</p>
-					<input type="text" className="form"/>
+					<input type="email" className="form" name="email"/>
 				</div>
 				<div className="field">
 					<p className="label">Password</p>
-					<input type="password" className="form" />
+					<input type="password" className="form" name="password"/>
 				</div>
 				<div id="register" onClick={() => onRouteChange('signin')}>Register</div>
 			</div>
--- src/Signin/Signin.js
+++ src/Signin/Signin.js
@@ -8,11 +8,11 @@ const App = ({route , onRouteChange}) => {
 				<div id="heading" >Welcome</div>
 				<div className="field">
 					<p className="label">Email</p>
-					<input type="text" className="form"/>
+					<input type="email" className="form" name="email"/>
 				</div>
 				<div className="field">
 					<p className="label">Password</p>
-					<input type="password" className="form" />
+					<input type="password" className="form" name="password"/>
 				</div>
 				<div id="signin" onClick={() => onRouteChange("home")}>Sign In</div>
 			</div>
