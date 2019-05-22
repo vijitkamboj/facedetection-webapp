2d8604469171475696c9bc97212fc628df995957
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-22 13:56:15 +0530

    Successfully created front-end

--- src/Logo/Logo.js
+++ src/Logo/Logo.js
@@ -5,7 +5,7 @@ import Tilt from 'react-tilt'
 const App = () => {
   return (
     <div id="logo">
-        <Tilt className="Tilt" options={{ max : 55 }} >
+        <Tilt className="Tilt" options={{ max : 80 }} >
            <div className="Tilt-inner"> </div>
         </Tilt>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App'
-import App2 from './app2'
+import App from './App'
+
 
 ReactDOM.render(
-    // <App />
-    <App2 />
+    <App />
 , document.getElementById('root'));
 
 
