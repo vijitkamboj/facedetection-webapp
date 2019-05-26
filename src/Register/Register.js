ea03bf1eb3e2c63d1fb7de545a2522d149c23bfc
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-27 02:02:18 +0530

    connected register

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,28 +1,66 @@
 import React from 'react';
 import './Register.css';
 
-const App = ({route , onRouteChange}) => {
-	if (route === 'register') {
-		return(
-			<div id = "form-cont">
-				<div id="heading" >Register</div>
-				<div className="field">
-					<p className="label">Name</p>
-					<input type="text" className="form" name="name"/>
+class App extends React.Component{
+	constructor(){
+		super();
+		this.state = {
+			registerEmail: '',
+			registerPass : '',
+			registerName : '',
+			error : ''
+		}
+	}
+
+	onEmailChange = (event) => {
+		this.setState({registerEmail : event.target.value})
+	}
+
+	onPassChange = (event) => {
+		this.setState({registerPass : event.target.value})
+	}
+
+	onNameChange = (event) => {
+		this.setState({registerName : event.target.value})
+	}
+
+	onSubmitRegister = () => {
+		fetch('http://localhost:3000/register' , {
+			method : 'post',
+			headers : {'Content-type' : 'application/json'},
+			body: JSON.stringify({
+				email : this.state.registerEmail,
+				password : this.state.registerPass,
+				name : this.state.registerName
+			})
+		})
+		this.props.onRouteChange('signin')
+	}
+
+	render(){
+		const {route} = this.props;
+		if (route === 'register') {
+			return(
+				<div id = "form-cont">
+					<div id="heading" >Register</div>
+					<div className="field">
+						<p className="label">Name</p>
+						<input type="text" className="form" name="name" onChange = {this.onNameChange}/>
+					</div>
+					<div className="field">
+						<p className="label">Email</p>
+						<input type="email" className="form" name="email" onChange = {this.onEmailChange}/>
+					</div>
+					<div className="field">
+						<p className="label">Password</p>
+						<input type="password" className="form" name="password" onChange = {this.onPassChange}/>
+					</div>
+					<div id="register" onClick={this.onSubmitRegister} >Register</div>
 				</div>
-				<div className="field">
-					<p className="label">Email</p>
-					<input type="email" className="form" name="email"/>
-				</div>
-				<div className="field">
-					<p className="label">Password</p>
-					<input type="password" className="form" name="password"/>
-				</div>
-				<div id="register" onClick={() => onRouteChange('signin')}>Register</div>
-			</div>
-		)
-	} else {
-		return null
+			)
+		} else {
+			return null
+		}
 	}
 }
 
